6dff34839de3e35734b4383a23d2da5bce872187
Jonathan Mangarret <jmangarret@gmail.com>
2023-01-29 17:50:37 -0500

    Fix deploy 3

--- src/commons/http.ts
+++ src/commons/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Movies } from "../interfaces/movie.interface";
 
-const URL_API = 'http://www.omdbapi.com/?i=tt3896198&apikey=5eec5adc&s=scary'
+const URL_API = 'https://www.omdbapi.com/?i=tt3896198&apikey=5eec5adc&s=scary'
 
 export async function getMovies(): Promise<Movies> {
         const { data, status } = await axios.get<Movies>(URL_API);
